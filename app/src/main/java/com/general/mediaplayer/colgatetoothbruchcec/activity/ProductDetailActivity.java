a6d70e721842a1ed98e8797ac145da8307642952
Jin Huang <jin.huang2017219@gmail.com>
2018-11-29 03:04:57 +0800

    Change press button

--- app/src/main/java/com/general/mediaplayer/colgatetoothbruchcec/activity/ProductDetailActivity.java
+++ app/src/main/java/com/general/mediaplayer/colgatetoothbruchcec/activity/ProductDetailActivity.java
@@ -50,6 +50,14 @@ public class ProductDetailActivity extends UsbSerialActivity {
     @BindView(R.id.btnPress)
     RoundKornerLinearLayout btnPress;
 
+    @BindView(R.id.txtPress)
+    TextView txtPress;
+
+    @BindView(R.id.txtFindBrash)
+    TextView txtFindBrash;
+
+    Boolean isPressBtnClicked = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,6 +72,9 @@ public class ProductDetailActivity extends UsbSerialActivity {
 
         Drawable drawable = ProductAdapter.loadDrawableFromAssets(this ,Global.currentProduct.product_image);
         thumbImageView.setImageDrawable(drawable);
+
+        txtPress.setText(getString(R.string.string_press));
+        txtFindBrash.setText(getString(R.string.string_find_brush));
 
         nameTextView.setText(Global.currentProduct.productname);
         descriptionTextView.setText(Global.currentProduct.long_description);
@@ -95,7 +106,17 @@ public class ProductDetailActivity extends UsbSerialActivity {
         btnPress.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendCommand(Global.currentProduct.section);
+                if (isPressBtnClicked) {
+                    isPressBtnClicked = false;
+                    txtPress.setText(getString(R.string.string_press));
+                    txtFindBrash.setText(getString(R.string.string_find_brush));
+                    onHome(v);
+                } else {
+                    isPressBtnClicked = true;
+                    sendCommand(Global.currentProduct.section);
+                    txtPress.setText(getString(R.string.string_return));
+                    txtFindBrash.setText(getString(R.string.string_main_menu));
+                }
             }
         });
     }
